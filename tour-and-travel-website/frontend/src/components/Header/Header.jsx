96d31148ae1f87b9a2761cb045b500b76bb37a3d
Subhamay-Dey <ncdey1966@gmail.com>
2024-07-05 19:01:47 +0530

    updated main.jsx - imported all the css dependencies in the main.jsx and using bootstrap instead of tailwind

--- tour-and-travel-website/frontend/src/components/Header/Header.jsx
+++ tour-and-travel-website/frontend/src/components/Header/Header.jsx
@@ -1,8 +1,36 @@
-import React from 'react'
+import React from 'react';
+import {Container, Row, Button} from "reactstrap";
+import {NavLink, Link} from "react-router-dom"
+import logo from "../../../public/react.svg"
+
+const nav__links = [
+  {
+    path: "/home",
+    display: "Home",
+  },
+  {
+    path: "#",
+    display: "About",
+  },
+  {
+    path: "/tours",
+    display: "Tours",
+  },
+]
 
 function Header() {
   return (
-    <div>Header</div>
+    <header className="header">
+      <Container>
+        <Row>
+          <div className='nav_wrapper d-flex items-center justify-between'>
+            <div className='logo'>
+              <img src={logo} alt="logo" />
+            </div>
+          </div>
+        </Row>
+      </Container>
+    </header>
   )
 }
 
--- tour-and-travel-website/frontend/src/main.jsx
+++ tour-and-travel-website/frontend/src/main.jsx
@@ -1,7 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css'
+import './index.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "remixicon/fonts/remixicon.css";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 import { BrowserRouter } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
@@ -9,5 +13,5 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <BrowserRouter>
       <App />
     </BrowserRouter>
-  </React.StrictMode>,
+  </React.StrictMode>
 )

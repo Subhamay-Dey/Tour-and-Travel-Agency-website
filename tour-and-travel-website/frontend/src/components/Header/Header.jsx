c43c158baceb237902a59aca406bbdb1b946042b
Subhamay-Dey <ncdey1966@gmail.com>
2024-07-05 21:10:45 +0530

    header component is updated and its looking good in website also coded the scss styling.

--- tour-and-travel-website/frontend/src/components/Header/Header.jsx
+++ tour-and-travel-website/frontend/src/components/Header/Header.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import {Container, Row, Button} from "reactstrap";
-import {NavLink, Link} from "react-router-dom"
-import logo from "../../../public/react.svg"
+import {NavLink, Link} from "react-router-dom";
+import logo from "../../../public/react.svg";
+import "./header.scss";
 
 const nav__links = [
   {
@@ -23,9 +24,29 @@ function Header() {
     <header className="header">
       <Container>
         <Row>
-          <div className='nav_wrapper d-flex items-center justify-between'>
+          <div className='nav__wrapper d-flex align-items-center justify-content-between'>
             <div className='logo'>
-              <img src={logo} alt="logo" />
+              <img src={logo} alt="logo"/>
+            </div>
+            <div className="navigation">
+              <ul className='menu d-flex align-items-center gap-5'>
+                {
+                  nav__links.map((item, index) => (
+                    <li className='nav__item' key={index}>
+                      <NavLink to={item.path}>{item.display}</NavLink>
+                    </li>
+                  ))
+                }
+              </ul>
+            </div>
+            <div className='nav__right d-flex align-items-center gap-4'>
+              <div className="nav__btns d-flex align-items-center gap-4">
+                <Button className='btn secondary__btn'><Link to="/login">Login</Link></Button>
+                <Button className='btn primary__btn'><Link to="/register">Register</Link></Button>
+              </div>
+              <span className='mobile__menu'>
+              <i class="ri-menu-line"></i>
+              </span>
             </div>
           </div>
         </Row>

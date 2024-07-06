f5d3669abe635b2ab86136366fbbf1a3520d28ae
Subhamay-Dey <ncdey1966@gmail.com>
2024-07-07 00:31:22 +0530

    Completed the Testimonial part of the website where I used a slider of react-slick.

--- tour-and-travel-website/frontend/src/components/Testimonial/Testimonials.jsx
+++ tour-and-travel-website/frontend/src/components/Testimonial/Testimonials.jsx
@@ -5,6 +5,36 @@ import ava02 from "../../images/ava-2.jpg";
 import ava03 from "../../images/ava-3.jpg";
 
 const Testimonials = () => {
+
+  const settings = {
+    dots: true,
+    infinite: true,
+    autoplay: true,
+    speed: 1000,
+    swipeToSlide: true,
+    autoplaySpeed: 2000,
+    slidesToShow: 3,
+
+    responsive:[
+      {
+        breakpoint: 992,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: true,
+        },
+      },
+      {
+        breakpoint: 576,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  }
+
   return (
     <>
       <Slider {...settings}>
@@ -29,7 +59,7 @@ const Testimonials = () => {
           <div className='d-flex align-items-center gap-4 mt-3'>
             <img src={ava02} alt="" className='w-25 h-25 rounded-2'/>
             <div>
-              <h6 className='mb-0 mt-3'>John Doe</h6>
+              <h6 className='mb-0 mt-3'>Lia Franklin</h6>
               <p>Customer</p>
             </div>
           </div>
@@ -43,6 +73,19 @@ const Testimonials = () => {
             <img src={ava03} alt="" className='w-25 h-25 rounded-2'/>
             <div>
               <h6 className='mb-0 mt-3'>John Doe</h6>
+              <p>Customer</p>
+            </div>
+          </div>
+        </div>
+        <div className="testimonial py-4 px-3">
+          <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iure facere temporibus, iusto consequuntur at ad ducimus nobis doloribus? Dignissimos velit in nisi dolore totam dolorem inventore maxime assumenda atque! Ab.
+          </p>
+
+          <div className='d-flex align-items-center gap-4 mt-3'>
+            <img src={ava02} alt="" className='w-25 h-25 rounded-2'/>
+            <div>
+              <h6 className='mb-0 mt-3'>Lia Franklin</h6>
               <p>Customer</p>
             </div>
           </div>

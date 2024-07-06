b95dc941d623c9d6bfc1bdd198d40aba46a6f83d
Subhamay-Dey <ncdey1966@gmail.com>
2024-07-06 23:58:30 +0530

    Successfullt all the three components inside the slider where the three customers reviews will be shown in the slider

--- tour-and-travel-website/frontend/src/components/Testimonial/Testimonials.jsx
+++ tour-and-travel-website/frontend/src/components/Testimonial/Testimonials.jsx
@@ -1,9 +1,54 @@
 import React from 'react'
 import Slider from "react-slick"
+import ava01 from "../../images/ava-1.jpg";
+import ava02 from "../../images/ava-2.jpg";
+import ava03 from "../../images/ava-3.jpg";
 
 const Testimonials = () => {
   return (
-    <div>Testimonial</div>
+    <>
+      <Slider {...settings}>
+        <div className="testimonial py-4 px-3">
+          <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iure facere temporibus, iusto consequuntur at ad ducimus nobis doloribus? Dignissimos velit in nisi dolore totam dolorem inventore maxime assumenda atque! Ab.
+          </p>
+
+          <div className='d-flex align-items-center gap-4 mt-3'>
+            <img src={ava01} alt="" className='w-25 h-25 rounded-2'/>
+            <div>
+              <h6 className='mb-0 mt-3'>John Doe</h6>
+              <p>Customer</p>
+            </div>
+          </div>
+        </div>
+        <div className="testimonial py-4 px-3">
+          <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iure facere temporibus, iusto consequuntur at ad ducimus nobis doloribus? Dignissimos velit in nisi dolore totam dolorem inventore maxime assumenda atque! Ab.
+          </p>
+
+          <div className='d-flex align-items-center gap-4 mt-3'>
+            <img src={ava02} alt="" className='w-25 h-25 rounded-2'/>
+            <div>
+              <h6 className='mb-0 mt-3'>John Doe</h6>
+              <p>Customer</p>
+            </div>
+          </div>
+        </div>
+        <div className="testimonial py-4 px-3">
+          <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iure facere temporibus, iusto consequuntur at ad ducimus nobis doloribus? Dignissimos velit in nisi dolore totam dolorem inventore maxime assumenda atque! Ab.
+          </p>
+
+          <div className='d-flex align-items-center gap-4 mt-3'>
+            <img src={ava03} alt="" className='w-25 h-25 rounded-2'/>
+            <div>
+              <h6 className='mb-0 mt-3'>John Doe</h6>
+              <p>Customer</p>
+            </div>
+          </div>
+        </div>
+      </Slider>
+    </>
   )
 }
 

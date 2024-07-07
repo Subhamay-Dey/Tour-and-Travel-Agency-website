e2ae73ef6b116ddb44ec14de14768882e2c0e9a2
Subhamay-Dey <ncdey1966@gmail.com>
2024-07-07 16:08:06 +0530

    footer part is updated and coded the footer.jsx file and the styling footer.scss

--- tour-and-travel-website/frontend/src/components/Footer/Footer.tsx
+++ tour-and-travel-website/frontend/src/components/Footer/Footer.tsx
@@ -1,8 +1,103 @@
-import React from 'react'
+import React from 'react';
+import "./footer.scss";
+
+import {Container, Row, Col, ListGroup, ListGroupItem} from "reactstrap";
+import {Link} from "react-router-dom";
+
+import logo from "../../images/logo.png";
+
+const footer__links = [
+  {
+    path: "/home",
+    display: "Home",
+  },
+  {
+    path: "/about",
+    display: "About",
+  },
+  {
+    path: "/tours",
+    display: "Tours",
+  },
+];
+
+const footer2__links = [
+  {
+    path: "/gallery",
+    display: "Gallery",
+  },
+  {
+    path: "/login",
+    display: "Login",
+  },
+  {
+    path: "/register",
+    display: "Register",
+  },
+]
 
 function Footer() {
   return (
-    <div>Footer</div>
+    <footer className='footer'>
+      <Container>
+        <Row>
+          <Col lg="3">
+          <div className="logo">
+            <img src={logo} alt="" />
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+            <div className="social__links d-flex align-items-center gap-4">
+              <span>
+                <Link to="#"><i className="ri-linkedin-box-fill"></i></Link>
+              </span>
+              <span>
+                <Link to="#"><i className="ri-instagram-line"></i></Link>
+              </span>
+              <span>
+                <Link to="#"><i className="ri-github-fill"></i></Link>
+              </span>
+              <span>
+                <Link to="#"><i className="ri-linkedin-box-fill"></i></Link>
+              </span>
+            </div>
+          </div>
+          </Col>
+          <Col lg="3">
+            <h5 className='footer__link-title'>Discover</h5>
+            <ListGroup className='footer__quick-links'>
+              {
+                footer__links.map((item, index) => (
+                  <ListGroupItem key={index} className='ps-0 border-0'>
+                    <Link to={item.path}>{item.display}</Link>
+                  </ListGroupItem>
+                ))
+              }
+            </ListGroup>
+          </Col>
+          <Col lg="3">
+          <h5 className='footer__link-title'>Quick Links</h5>
+            <ListGroup className='footer__quick-links'>
+              {
+                footer2__links.map((item, index) => (
+                  <ListGroupItem key={index} className='ps-0 border-0'>
+                    <Link to={item.path}>{item.display}</Link>
+                  </ListGroupItem>
+                ))
+              }
+            </ListGroup>
+          </Col>
+          <Col lg="3">
+          <h5 className='footer__link-title'>Contact Us</h5>
+            <ListGroup className='footer__quick-links'>
+                  <ListGroupItem  className='ps-0 border-0 d-flex align-items-center gap-4'>
+                    <h6 className='mb-0 d-flex align-items-center gap-3'>
+                      <span><i className="ri-map-pin-line"></i></span>
+                      Address:
+                    </h6>
+                  </ListGroupItem>
+            </ListGroup></Col>
+        </Row>
+      </Container>
+    </footer>
   )
 }
 
